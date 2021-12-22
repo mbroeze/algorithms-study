d2522aaa3a4bcffe5efcd792b1b858f7975057b0
Mathew Broeze <broe0027@algonquinlive.com>
2021-12-22 06:54:03 -0500

    updated insertion sort overview; info re: loop invariants as well

--- implementations/src/sort/InsertionSort.java
+++ implementations/src/sort/InsertionSort.java
@@ -5,6 +5,28 @@ package sort;
  * then we can order the first k+1 elements by shifting all elements greater than the k+1th element up one place
  * in the sequence, and inserting the previous k+1th element at the previous place of the last element that was shifted.
  *
+ * We can formalize this rough mathematical induction with loop invariants. A loop invariant for an algorithm must satisfy three
+ * properties:
+ * - initialization: the loop invariant is true prior to the first iteration of the loop
+ * - maintenance: if the loop invariant is true before the iteration of the loop, then it is true after
+ * - termination: upon loop termination, the loop invariant gives us a useful property to demonstrate correctness
+ *
+ * The insertion sort iterates over each element from the second to the last. The loop invariant for each
+ * index idx is that the elements at indices 0,1,...,idx-1
+ *  i) are the same elements as were originally in the array
+ *  ii) are in sorted order
+ * Considering the insertion sort algorithm...
+ * - initialization: the element at index 0 is not changed, and a single element is in sorted order.
+ * - termination: the algorithm terminates when idx is greater than the length of the array; the loop invariant
+ *   guarantees that all previous elements are sorted, so the array is sorted.
+ *
+ * Maintenance is a little more tricky as it requires considering a loop invariant for the algorithm that shifts each
+ * element up by one. This algorithm considers an element E at idx, and begins at jdx=idx-1.
+ * I <i>suspect</i> the loop invariant for this algorithm is something along the lines of:
+ * - the elements at [0,jdx) (possibly empty when jdx=0) are sorted and consist of the elements originally at [0,jdx),
+ * - the elements at (jdx, idx] (possibly empty when jdx=idx) are sorted, and consists of the elements originally at [jdx, idx)
+ * - E is less than each of the elements at (jdx,idx].
+ *
  * Insertion sort is in O(n^2), and is efficient for a small number of elements.
  */
 public class InsertionSort {
@@ -28,6 +50,7 @@ public class InsertionSort {
                 inputArray[jdx + 1] = inputArray[jdx];
                 jdx--;
             }
+
             // insert the current element; add one to jdx to account for decrement in last while loop (or initialization of jdx)
             inputArray[jdx + 1] = currentElement;
         }
